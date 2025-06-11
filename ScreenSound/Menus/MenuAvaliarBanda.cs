562ab2fa76d7276a371edbd5a37654fc9deaaa82
Bruno Perez <braphaelperez98@gmail.com>
2025-06-11 18:23:34 -0300

    Criadas classes para cada opção do menu

---
+++ ScreenSound/Menus/MenuAvaliarBanda.cs
@@ -0,0 +1,25 @@
+using ScreenSound.Modelos;
+namespace ScreenSound.Menus;
+internal class MenuAvaliarBanda: Menu
+{
+    public void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloDaOpcao("Avaliar banda");
+        Console.Write("Digite o nome da banda que deseja avaliar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];//puxar a chave da banda no dicionario.
+            Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+
+            Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);//criando objeto nota para o contexto atual
+            banda.AdicionarNota(nota);//adicionando nota no metodo da banda
+
+            Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+        }
+    }
+}
